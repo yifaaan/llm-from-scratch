32c295867fb54b2d65461aea5b21da2a9405ff55
yifaaan <0219lyf@gmail.com>
2025-06-22 09:37:02 +0800

    read tensor weights

---
+++ enc.py
@@ -0,0 +1,32 @@
+import struct
+import tiktoken
+
+with open("tiny.txt") as f:
+    enc = tiktoken.get_encoding("gpt2")
+    tokens = enc.encode(f.read())
+
+with open("data", "wb") as f_d:
+    for i in tokens:
+        v = struct.pack("H", i)
+        f_d.write(v)
+
+structs = []
+words = b""
+
+# get vocab table
+for i in range(50257):
+    # word = enc.decode([i])
+    word = enc.decode_single_token_bytes(i)
+    offset = len(words)
+    structs.append(offset)
+    size = len(word)
+    structs.append(size)
+    # words += bytes(word, "utf-8")
+    words += word
+
+with open("enc", "wb") as f_out:
+    for st in structs:
+        v = struct.pack("I", st)
+        f_out.write(v)
+    f_out.write(words)
+print(len(words))
---
+++ src/gpt2.cpp
@@ -0,0 +1,164 @@
+#include <array>
+#include <cstdint>
+#include <fmt/format.h>
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <span>
+#include <vector>
+
+constexpr size_t AlignTo256(size_t size) {
+  return (size + 255) & ~size_t(0xff);
+}
+
+template <typename T> constexpr size_t AlignedSize() {
+  return AlignTo256(sizeof(T));
+}
+
+constexpr auto VocabSize = 50257;
+// constexpr auto VocabLength = 321428;
+constexpr auto VocabLength = 320827;
+constexpr auto TextTokenLength = 338024;
+
+struct Token {
+  uint32_t offset;
+  uint32_t size;
+};
+
+struct Data {
+  std::array<uint16_t, TextTokenLength> text_token_ids;
+};
+
+struct LayerParams {
+  float *weight{};
+  float *bias{};
+};
+
+struct AttentionParams {
+  LayerParams c_attn;
+  LayerParams c_proj;
+};
+
+struct MlpParams {
+  LayerParams c_fc;
+  LayerParams c_proj;
+};
+
+struct TransformerBlock {
+  LayerParams ln_1;
+  AttentionParams attn;
+  LayerParams ln_2;
+  MlpParams mlp;
+};
+
+struct Parameters {
+  LayerParams wte;
+  LayerParams wpe;
+  std::array<TransformerBlock, 12> headers;
+  LayerParams ln_f;
+};
+
+class Decoder {
+public:
+  Decoder() {
+    tokens.resize(VocabSize);
+    raw.resize(VocabLength);
+    std::ifstream enc{"enc", std::ios::binary};
+    enc.read(reinterpret_cast<char *>(tokens.data()),
+             VocabSize * sizeof(Token));
+    enc.read(raw.data(), VocabLength);
+  }
+
+  void Print() {
+    for (const auto &token : tokens) {
+      std::cout << fmt::format(
+          "offset:{}, size:{}, token:{}\n", token.offset, token.size,
+          std::string_view{raw.data() + token.offset, token.size});
+    }
+  }
+
+  std::string TokensToString(std::span<uint16_t> ids) {
+    std::string result;
+    for (auto id : ids) {
+      // std::cout << fmt::format(
+      //     "{}->{}\n", id,
+      //     std::string_view{raw.data() + tokens[id].offset, tokens[id].size});
+      result +=
+          std::string_view{raw.data() + tokens[id].offset, tokens[id].size};
+    }
+    // std::cout << result << '\n';
+    return result;
+  }
+
+private:
+  // vocab table
+  std::vector<Token> tokens;
+  // text
+  std::string raw;
+};
+
+int main() {
+
+  Decoder decode;
+  // decode.Print();
+
+  std::ifstream f_data{"data", std::ios::binary};
+  std::vector<uint16_t> text_token_ids(TextTokenLength);
+  f_data.read(reinterpret_cast<char *>(text_token_ids.data()),
+              TextTokenLength * 2);
+  decode.TokensToString(text_token_ids);
+
+  std::ifstream f_weight{"model.safetensors", std::ios::binary};
+  uint64_t json_size;
+  f_weight.read(reinterpret_cast<char *>(&json_size), 8);
+
+  std::string json_str;
+  json_str.resize(json_size);
+  f_weight.read(reinterpret_cast<char *>(json_str.data()), json_size);
+  fmt::print("{}\n", json_str);
+
+  auto safetensors_json = nlohmann::json::parse(json_str);
+
+  // get tensor data start
+  auto tensor_data_start = f_weight.tellg();
+  f_weight.seekg(0, std::ios::end);
+  auto file_end = f_weight.tellg();
+  auto tensor_data_size = file_end - tensor_data_start;
+  f_weight.seekg(tensor_data_start);
+
+  std::vector<char> tensor_data(tensor_data_size);
+  f_weight.read(tensor_data.data(), tensor_data_size);
+  auto tensor_base_ptr = tensor_data.data();
+
+  auto get_tensor = [&](std::string_view key) -> float * {
+    if (safetensors_json.contains(key)) {
+      auto start = safetensors_json[key]["data_offsets"][0].get<uint16_t>();
+      return reinterpret_cast<float *>(tensor_base_ptr + start);
+    }
+    return nullptr;
+  };
+  Parameters params;
+  params.wte.weight = get_tensor("wte.weight");
+  params.wpe.weight = get_tensor("wpe.weight");
+  for (int i = 0; i < 12; i++) {
+    params.headers[i].ln_1.weight = get_tensor(fmt::format("h.{}.ln_1.weight", i));
+    params.headers[i].ln_1.bias = get_tensor(fmt::format("h.{}.ln_1.bias", i));
+    params.headers[i].attn.c_attn.weight = get_tensor(fmt::format("h.{}.attn.c_attn.weight", i));
+    params.headers[i].attn.c_attn.bias = get_tensor(fmt::format("h.{}.attn.c_attn.bias", i));
+    params.headers[i].attn.c_proj.weight = get_tensor(fmt::format("h.{}.attn.c_proj.weight", i));
+    params.headers[i].attn.c_proj.bias = get_tensor(fmt::format("h.{}.attn.c_proj.bias", i));
+    params.headers[i].ln_2.weight = get_tensor(fmt::format("h.{}.ln_2.weight", i));
+    params.headers[i].ln_2.bias = get_tensor(fmt::format("h.{}.ln_2.bias", i));
+    params.headers[i].mlp.c_fc.weight = get_tensor(fmt::format("h.{}.mlp.c_fc.weight", i));
+    params.headers[i].mlp.c_fc.bias = get_tensor(fmt::format("h.{}.mlp.c_fc.bias", i));
+    params.headers[i].mlp.c_proj.weight = get_tensor(fmt::format("h.{}.mlp.c_proj.weight", i));
+    params.headers[i].mlp.c_proj.bias = get_tensor(fmt::format("h.{}.mlp.c_proj.bias", i));
+  }
+  params.ln_f.weight = get_tensor("ln_f.weight");
+  params.ln_f.bias = get_tensor("ln_f.bias");
+
+  std::cout << *params.ln_f.weight << std::endl;
+  std::cout << *params.ln_f.bias << std::endl;
+  std::cout << *params.wte.weight << std::endl;
+  std::cout << *params.wpe.weight << std::endl;
+}
