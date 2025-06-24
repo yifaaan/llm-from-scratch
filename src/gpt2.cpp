049647c260da0965e27125938de3ddc6b084c3bd
yifaaan <0219lyf@gmail.com>
2025-06-24 08:30:01 +0800

    we predit the next token successfully

--- src/gpt2.cpp
+++ src/gpt2.cpp
@@ -16,15 +16,15 @@ constexpr size_t AlignedSize() {
     return AlignTo256(sizeof(T));
 }
 
-constexpr auto VocabSize = 50257;
-constexpr auto DModel = 768;
-constexpr auto DSeq = 1024;
-constexpr auto DK = DModel / 12;
+constexpr auto VOCAB_SIZE = 50257;
+constexpr auto D_MODEL = 768;
+constexpr auto D_SEQ = 1024;
+constexpr auto DK = D_MODEL / 12;
 constexpr auto EPS = 1e-5f;
 
 // constexpr auto VocabLength = 321428;
-constexpr auto VocabLength = 320827;
-constexpr auto TextTokenLength = 338024;
+constexpr auto VOCAB_LENGTH = 320827;
+constexpr auto TEXT_TOKEN_LENGTH = 338024;
 
 struct Token {
     uint32_t offset;
@@ -32,7 +32,7 @@ struct Token {
 };
 
 struct Data {
-    std::array<uint16_t, TextTokenLength> text_token_ids;
+    std::array<uint16_t, TEXT_TOKEN_LENGTH> text_token_ids;
 };
 
 struct LayerParams {
@@ -66,59 +66,61 @@ struct Parameters {
 
 struct Activation {
     // Token embedding + position embedding
-    float embedding_out[DSeq][DModel];
+    float embedding_out[D_SEQ][D_MODEL];
 
     struct TransformerBlock {
         // LayerNorm 1
-        float ln_1_out[DSeq][DModel];
+        float ln_1_out[D_SEQ][D_MODEL];
         // Self-Attention: Q, K, V
         //  每个头的维度 | d_k = 64 (隐式) | head_dim = 64 (需要定义) | 64 | DModel
         //  num_heads，即 768 / 12。每个头的 Q, K, V 向量都是64维。
-        float attn_c_attn_out[DSeq][DModel * 3];
+        float attn_c_attn_out[D_SEQ][D_MODEL * 3];
 
         // float attn_q[DSeq][DModel];
         // float attn_k[DSeq][DModel];
         // float attn_v[DSeq][DModel];
 
         // logits: Q @ K: [DSeq, DSeq]
-        float attn_logits_out[12][DSeq][DSeq];
+        float attn_logits_out[12][D_SEQ][D_SEQ];
 
-        float attn_softmax_out[12][DSeq][DSeq];
+        float attn_softmax_out[12][D_SEQ][D_SEQ];
         // Attention score: [DSeq, DSeq]
-        float attn_z_out[DSeq][DModel];
+        float attn_z_out[D_SEQ][D_MODEL];
 
         // Attention output: [DSeq, DModel]
-        float attn_c_proj_out[DSeq][DModel];
+        float attn_c_proj_out[D_SEQ][D_MODEL];
 
         // Shortcut connection: [DSeq, DModel] = embedding_out + attn_c_proj_out
-        float res_1_out[DSeq][DModel];
+        float res_1_out[D_SEQ][D_MODEL];
 
         // LayerNorm 2
-        float ln_2_out[DSeq][DModel];
+        float ln_2_out[D_SEQ][D_MODEL];
 
         // mlp_c_fc_out
-        float mlp_c_fc_out[DSeq][DModel * 4];
+        float mlp_c_fc_out[D_SEQ][D_MODEL * 4];
         // gelu
-        float mlp_gelu_out[DSeq][DModel * 4];
+        float mlp_gelu_out[D_SEQ][D_MODEL * 4];
         // mlp_c_proj_out
-        float mlp_c_proj_out[DSeq][DModel];
+        float mlp_c_proj_out[D_SEQ][D_MODEL];
         // Shortcut connection: [DSeq, DModel] = attn_c_proj_out + mlp_c_proj_out
-        float res_2_out[DSeq][DModel];
+        float res_2_out[D_SEQ][D_MODEL];
     };
 
     std::array<TransformerBlock, 12> blocks;
 
+    float ln_f_out[D_SEQ][D_MODEL];
+    float y_pred[D_SEQ][VOCAB_LENGTH];
     // Final layer norm
 };
 
 class Decoder {
 public:
     Decoder() {
-        tokens.resize(VocabSize);
-        raw.resize(VocabLength);
+        tokens.resize(VOCAB_SIZE);
+        raw.resize(VOCAB_LENGTH);
         std::ifstream enc{"enc", std::ios::binary};
-        enc.read(reinterpret_cast<char*>(tokens.data()), VocabSize * sizeof(Token));
-        enc.read(raw.data(), VocabLength);
+        enc.read(reinterpret_cast<char*>(tokens.data()), VOCAB_SIZE * sizeof(Token));
+        enc.read(raw.data(), VOCAB_LENGTH);
     }
 
     void Print() {
@@ -242,8 +244,8 @@ int main() {
 
     // The text data
     std::ifstream f_data{"data", std::ios::binary};
-    std::vector<uint16_t> text_token_ids(TextTokenLength);
-    f_data.read(reinterpret_cast<char*>(text_token_ids.data()), TextTokenLength * 2);
+    std::vector<uint16_t> text_token_ids(TEXT_TOKEN_LENGTH);
+    f_data.read(reinterpret_cast<char*>(text_token_ids.data()), TEXT_TOKEN_LENGTH * 2);
     decode.TokensToString(text_token_ids);
 
     std::ifstream f_weight{"model.safetensors", std::ios::binary};
@@ -301,11 +303,11 @@ int main() {
     for (size_t i = 0; i < input_size; i++) {
         auto token = text_token_ids[i];
         // Token embedding vec
-        auto wte = params.wte.weight + token * DModel;
+        auto wte = params.wte.weight + token * D_MODEL;
         // Pos embedding vec
-        auto wpe = params.wpe.weight + i * DModel;
+        auto wpe = params.wpe.weight + i * D_MODEL;
         auto embedding_out = activation->embedding_out[i];
-        for (size_t j = 0; j < DModel; j++) {
+        for (size_t j = 0; j < D_MODEL; j++) {
             embedding_out[j] = wte[j] + wpe[j];
         }
     }
@@ -320,7 +322,7 @@ int main() {
             params.headers[layer_i].ln_1.weight,
             params.headers[layer_i].ln_1.bias,
             reinterpret_cast<float*>(activation->blocks[layer_i].ln_1_out),
-            DModel,
+            D_MODEL,
             input_size);
 
         full_connect(
@@ -328,8 +330,8 @@ int main() {
             params.headers[layer_i].attn.c_attn.weight,
             params.headers[layer_i].attn.c_attn.bias,
             reinterpret_cast<float*>(activation->blocks[layer_i].attn_c_attn_out),
-            DModel,
-            DModel * 3,
+            D_MODEL,
+            D_MODEL * 3,
             input_size);
 
         for (size_t head_idx = 0; head_idx < 12; head_idx++) {
@@ -345,7 +347,7 @@ int main() {
                 // All the tokens before the q_i-th token
                 for (size_t k_i = 0; k_i <= q_i; k_i++) {
                     // k_i-th token's k_vec:[DK=64]
-                    auto k = activation->blocks[layer_i].attn_c_attn_out[k_i] + DModel + head_idx * DK;
+                    auto k = activation->blocks[layer_i].attn_c_attn_out[k_i] + D_MODEL + head_idx * DK;
                     float sum = 0.0f;
                     for (size_t j = 0; j < DK; j++) {
                         sum += q[j] * k[j];
@@ -381,7 +383,7 @@ int main() {
                     // attn_softmax_out[head_idx][q_i][q_i, q_i+1, ..., input_size] is zero
                     for (size_t k_i = 0; k_i <= q_i; k_i++) {
                         // k_i-th token's v_token_vec
-                        auto v_token = activation->blocks[layer_i].attn_c_attn_out[k_i] + DModel * 2 + head_idx * DK;
+                        auto v_token = activation->blocks[layer_i].attn_c_attn_out[k_i] + D_MODEL * 2 + head_idx * DK;
                         sum += s_token[k_i] * v_token[j];
                     }
                     activation->blocks[layer_i].attn_z_out[q_i][head_idx * DK + j] = sum;
@@ -394,8 +396,8 @@ int main() {
             params.headers[layer_i].attn.c_proj.weight,
             params.headers[layer_i].attn.c_proj.bias,
             reinterpret_cast<float*>(activation->blocks[layer_i].attn_c_proj_out),
-            DModel,
-            DModel,
+            D_MODEL,
+            D_MODEL,
             input_size);
 
         {
@@ -404,7 +406,7 @@ int main() {
             auto in_1 = layer_in;
             auto in_2 = reinterpret_cast<float*>(activation->blocks[layer_i].attn_c_proj_out);
             auto out = reinterpret_cast<float*>(activation->blocks[layer_i].res_1_out);
-            auto out_end = out + input_size * DModel;
+            auto out_end = out + input_size * D_MODEL;
             for (; out != out_end; in_1++, in_2++, out++) {
                 *out = *in_1 + *in_2;
             }
@@ -415,7 +417,7 @@ int main() {
             params.headers[layer_i].ln_2.weight,
             params.headers[layer_i].ln_2.bias,
             reinterpret_cast<float*>(activation->blocks[layer_i].ln_2_out),
-            DModel,
+            D_MODEL,
             input_size);
 
         full_connect(
@@ -423,15 +425,15 @@ int main() {
             params.headers[layer_i].mlp.c_fc.weight,
             params.headers[layer_i].mlp.c_fc.bias,
             reinterpret_cast<float*>(activation->blocks[layer_i].mlp_c_fc_out),
-            DModel,
-            DModel * 4,
+            D_MODEL,
+            D_MODEL * 4,
             input_size);
 
         {
             // mlp_c_fc_out_gelu
             auto in = reinterpret_cast<float*>(activation->blocks[layer_i].mlp_c_fc_out);
             auto out = reinterpret_cast<float*>(activation->blocks[layer_i].mlp_gelu_out);
-            auto out_end = out + input_size * DModel * 4;
+            auto out_end = out + input_size * D_MODEL * 4;
             for (; out != out_end; in++, out++) {
                 auto x = *in;
                 // x * 0.5f * (1 + std::erff(x / std::sqrt(2)));
@@ -445,39 +447,68 @@ int main() {
             params.headers[layer_i].mlp.c_proj.weight,
             params.headers[layer_i].mlp.c_proj.bias,
             reinterpret_cast<float*>(activation->blocks[layer_i].mlp_c_proj_out),
-            DModel * 4,
-            DModel,
+            D_MODEL * 4,
+            D_MODEL,
             input_size);
 
         {
             auto in_1 = reinterpret_cast<float*>(activation->blocks[layer_i].res_1_out);
             auto in_2 = reinterpret_cast<float*>(activation->blocks[layer_i].mlp_c_proj_out);
             auto out = reinterpret_cast<float*>(activation->blocks[layer_i].res_2_out);
-            auto out_end = out + input_size * DModel;
+            auto out_end = out + input_size * D_MODEL;
             for (; out != out_end; in_1++, in_2++, out++) {
                 *out = *in_1 + *in_2;
             }
         }
     }
 
+    layer_norm(
+        reinterpret_cast<float*>(activation->blocks[11].res_2_out),
+        params.ln_f.weight,
+        params.ln_f.bias,
+        reinterpret_cast<float*>(activation->ln_f_out),
+        D_MODEL,
+        input_size);
+
+    // Here, the wte.weight's shape is [VocabSize, DModel]. Acutally we expect [DModel, VocabSize].
+
+    for (size_t i = 0; i < input_size; i++) {
+        auto in = activation->ln_f_out[i];
+        auto out = activation->y_pred[i];
+        // ln_f_out[i] * W.T
+        for (size_t j = 0; j < VOCAB_SIZE; j++) {
+            float dot = 0.0f;
+            const auto weight = params.wte.weight + j * D_MODEL;
+            for (size_t k = 0; k < D_MODEL; k++) {
+                dot += in[k] * weight[k];
+            }
+            out[j] = dot;
+        }
+    }
+
+    // Predict the next token
+    const auto row = activation->y_pred[input_size - 1];
+    const auto target_idx = std::distance(row, std::max_element(row, row + VOCAB_SIZE));
+    std::cout << fmt::format("target token id:{}", target_idx);
+
     // {
     //     float sum = 0.0f;
     //     for (size_t k = 0; k < input_size; k++) {
+    //         for (size_t j = 0; j < VOCAB_SIZE; j++) {
+    //             sum += activation->y_pred[k][j];
+    //         }
+    //     }
+    //     std::cout << fmt::format("sum:{}\n", sum);
+    // }
+
+    // // For test
+    // for (size_t i = 0; i < 12; i++) {
+    //     float sum = 0.0f;
+    //     for (size_t k = 0; k < input_size; k++) {
     //         for (size_t j = 0; j < DModel; j++) {
-    //             sum += activation->blocks[0].res_2_out[k][j];
+    //             sum += activation->blocks[i].res_2_out[k][j];
     //         }
     //     }
     //     std::cout << fmt::format("layer:{}, sum:{}\n", i, sum);
     // }
-
-    // For test
-    for (size_t i = 0; i < 12; i++) {
-        float sum = 0.0f;
-        for (size_t k = 0; k < input_size; k++) {
-            for (size_t j = 0; j < DModel; j++) {
-                sum += activation->blocks[i].res_2_out[k][j];
-            }
-        }
-        std::cout << fmt::format("layer:{}, sum:{}\n", i, sum);
-    }
 }
